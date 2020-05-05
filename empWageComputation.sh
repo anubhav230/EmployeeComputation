8ccf184940af397cd1b80291ab4ef96b8787697e
anubhav230 <anubhavsingh230@gmail.com>
2020-05-05 14:04:19 +0530

    function has added for salary calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,30 +1,35 @@
 #!/bin/bash -x
-echo "Welcome to employee computation program::"
-isPartTime=1;
-isFullTime=2;
-maxHrsInMonth=10;
-empRatePerHr=20;
-numWorkingDays=20;
 
-totalEmpHr=0;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalWorkHours=0;
 totalWorkingDays=0;
-while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
-do
-        ((totalWorkingDays++))
-        empCheck=$((RANDOM%3))
-        case $empCheck in
-                $isFullTime)
-                        emoHrs=8
+
+function getWorkingHours() {
+          case $1 in
+                $IS_FULL_TIME)
+                        workHours=8
                         ;;
-                $isPartTime)
-                        empHrs=4
+                $IS_PART_TIME)
+                        workHours=4
                         ;;
                 *)
-                        empHrs=0
-                        ;;
+                        workHours=0
+  ;;
         esac
-        totalEmpHr=$(($totalEmpHr+$empHrs))
-done
-totalSalary=$(($totalEmpHr*$empRatePerHr))
+}
 
-echo "$totalSalary"
+while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH &&
+        $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+        ((totalWorkingDays++))
+        workHours="$( getWorkingHours $((RANDOM%3)) )";
+        totalWorkHours=$((totalWorkHours + workHours))
+
+done
+
+totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR));
